0afc2d86254a1d5f30f22933195e9fe92ed30358
Alejandro Rey <alejandroreyc@gmail.com>
2023-06-05 14:43:08 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Articulos from './components/Articulos';
+
+function App() {
+  return (
+    <div>
+      <Articulos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Articulos.js
@@ -0,0 +1,53 @@
+import React, {  useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+
+import ListadoArticulos from './ListadoArticulos'
+
+const Articulos = () => {
+  const { register, handleSubmit } = useForm();
+  
+  const [lista, setLista] = useState(null);
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/articulos', {
+        params: data
+      });
+      
+      console.log(response.data)
+
+      setLista(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Formulario de Búsqueda</h1>
+      <div className="card mb-3">
+        <div className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3">
+              <label className="form-label">Nombre:</label>
+              <input type="text" className="form-control" {...register('nombre')} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Activo:</label>
+              <select className="form-select" {...register('activo')}>
+                <option value="">Todos</option>
+                <option value="true">Sí</option>
+                <option value="false">No</option>
+              </select>
+            </div>
+            <button type="submit" className="btn btn-primary">Buscar</button>
+          </form>
+        </div>
+      </div>
+      {lista && <ListadoArticulos lista={lista} />}
+    </div>
+  );
+};
+
+export default Articulos;
